60f748816bf5a6c4915c65a5609b47336d1c00df
Sergio <srosduran@gmail.com>
2021-03-13 17:52:31 +0100

    finish section

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      // fullname: ''
     };
+  },
+  watch: {
+    counter(value) {
+      if(value > 50) this.counter = 0;
+    }
+    // name(value) {
+    //   value === '' ? this.fullname = ''
+    //   : this.fullname = value + ' ' + this.lastName;
+    // },
+    // lastName(value) {
+    //   value === '' ? this.fullname = ''
+    //   : this.fullname = this.name + ' ' + value;
+    // }
   },
   computed: {
     fullname(){
-      if(this.name === '') return ''
-      else return this.name + ' ' + 'Ros'; 
+      if(this.name === '' || this.lastName === '') return ''
+      else return this.name + ' ' + this.lastName; 
     }
   },
   methods: {
@@ -24,11 +39,12 @@ const app = Vue.createApp({
     },
     resetInput(){
       this.name = '';
+      this.lastName = '';
     },
-    outputFullname(){
-      if(this.name === '') return ''
-      else return this.name + ' ' + 'Ros';
-    }
+    // outputFullname(){
+    //   if(this.name === '') return ''
+    //   else return this.name + ' ' + 'Ros';
+    // }
   }
 });
 
